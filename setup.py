4e4e1a651dc9dd6f488170e36f00bb9b0dff3a75
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2023-04-29 14:37:45 +0800

    alvistack/v13.5.2
        git clean -xdf
        tar zcvf ../python-rich_13.5.2.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-rich.spec ../python-rich_13.5.2-1.spec
        mv ../python*-rich*13.5.2*.{gz,xz,spec,dsc} /osc/home\:alvistack/Textualize-rich-13.5.2/
        rm -rf ../python*-rich*13.5.2*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,34 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
 
-# This is a shim to hopefully allow Github to detect the package, build is done with poetry
+packages = \
+['rich']
 
-import setuptools
+package_data = \
+{'': ['*']}
 
-if __name__ == "__main__":
-    setuptools.setup(name="rich")
+install_requires = \
+['markdown-it-py>=2.2.0', 'pygments>=2.13.0,<3.0.0']
+
+extras_require = \
+{':python_version < "3.9"': ['typing-extensions>=4.0.0,<5.0'],
+ 'jupyter': ['ipywidgets>=7.5.1,<9']}
+
+setup_kwargs = {
+    'name': 'rich',
+    'version': '13.5.2',
+    'description': 'Render rich text, tables, progress bars, syntax highlighting, markdown and more to the terminal',
+    'author': 'Will McGugan',
+    'author_email': 'willmcgugan@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/Textualize/rich',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.7.0',
+}
+
+
+setup(**setup_kwargs)
